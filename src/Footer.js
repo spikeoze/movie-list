29b366a4d99744501e8586ca6ae7b316630008bc
Mukhtar Amin <mukhtaaramiin@gmail.com>
2022-02-01 15:33:10 +0300

    open source code in another tap

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <div className='footer-content'>
       <h4>made by mukhtar with react</h4>
       <p>Source code: 
-      <a href="https://github.com/spikeoze/movie-list"><FaGithub className='logo'/></a></p>
+      <a href="https://github.com/spikeoze/movie-list" target="_blank"><FaGithub className='logo'/></a></p>
     </div>
   </section>;
 }
